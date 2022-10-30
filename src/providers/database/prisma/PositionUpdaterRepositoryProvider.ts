36b11d5004f87d504ac655e4aa54d39e37a844da
Aviv Ben-Yosef <aviv@interactech.com>
2022-10-30 17:14:55 +0200

    added missing await @ Position updater repository provider

--- src/providers/database/prisma/PositionUpdaterRepositoryProvider.ts
+++ src/providers/database/prisma/PositionUpdaterRepositoryProvider.ts
@@ -3,7 +3,7 @@ import { IPositionUpdaterRepositoryProvider } from "../../../contracts/interface
 import { PositionStatuses } from "../../../contracts/PositionStatuses";
 class PositionUpdaterRepositoryProvider extends BaseRepositoryProvider implements IPositionUpdaterRepositoryProvider {
     async update(id: number, options: { status?: PositionStatuses | undefined; name?: string | undefined; description?: string | undefined; }): Promise<void> {
-        this._prisma.position.update({
+        await this._prisma.position.update({
             where: {id: id},
             data: options
         })
